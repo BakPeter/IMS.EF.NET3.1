a606bf061e3c702411dae79bfc227122b2dce966
BakPeter <bak.pet@gmail.com>
2021-10-25 22:30:06 +0300

    Service Response implemented

---
+++ IMS.EF.NET3.1/ServiceResponse/ServiceResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceResponse
+{
+    public class ServiceResponse<T>
+    {
+        public T Data { get; set; }
+        public bool Succsees { get; set; } = true;
+        public string Message { get; set; } = null;
+    }
+}
