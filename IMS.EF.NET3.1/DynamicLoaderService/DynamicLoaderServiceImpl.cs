2b01d48d76b271bfc152c14685f3dd4b76a1c1ee
BakPeter <bak.pet@gmail.com>
2021-10-25 18:34:16 +0300

    DynamicLoaderService Implemented

---
+++ IMS.EF.NET3.1/DynamicLoaderService/DynamicLoaderServiceImpl.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace DynamicLoaderService
+{
+    public class DynamicLoaderServiceImpl
+    {
+        public void LoadServices(IServiceCollection services, string path)
+        {
+            var servicePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path);
+
+            var files = Directory.GetFiles(servicePath, "*.dll");
+            foreach (var file in files)
+            {
+                var assembly = Assembly.LoadFile(file);
+                foreach (var type in assembly.GetTypes())
+                {
+                    var loaderAttribute = type.GetCustomAttribute<LoaderAttribute>();
+                    if (loaderAttribute != null)
+                    {
+                        switch (loaderAttribute.Policy)
+                        {
+                            case Policy.Transient:
+                                services.AddTransient(loaderAttribute.InterfaceType, loaderAttribute.ImplementationType);
+                                break;
+                            case Policy.Scoped:
+                                services.AddScoped(loaderAttribute.InterfaceType, loaderAttribute.ImplementationType);
+                                break;
+                            case Policy.Singelton:
+                                services.AddSingleton(loaderAttribute.InterfaceType, loaderAttribute.ImplementationType);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ IMS.EF.NET3.1/DynamicLoaderService/LoaderAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicLoaderService
+{
+    public enum Policy
+    {
+        Transient,
+        Scoped,
+        Singelton
+    }
+
+    public class LoaderAttribute : Attribute
+    {
+        public Type InterfaceType { get; private set; }
+        public Type ImplementationType { get; private set; }
+        public Policy Policy { get; set; }
+    }
+}
