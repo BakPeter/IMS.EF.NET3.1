ffbee0e768ecf423137b5d32bdf34266b7a3256d
BakPeter <bak.pet@gmail.com>
2021-10-27 23:32:33 +0300

    GetUser microservice implemented

---
+++ IMS.EF.NET3.1/IMS_Services_GetUser/Controllers/GetUserController.cs
@@ -0,0 +1,42 @@
+﻿using IMS_DAL_GstUserContracts;
+using IMS_DTO_GetUser;
+using LoggingUtils;
+using Microsoft.AspNetCore.Mvc;
+using ServiceResponse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TemplateMethodContracts;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IMS_Services_GetUser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetUserController : ControllerBase
+    {
+        private readonly IDalGetUserService _getUserService;
+        private readonly ITemplateMethodParamService<ServiceResponse<GetUserResDTO>, GetUserReqDTO> _templateLoggingService;
+
+        public GetUserController(
+            IDalGetUserService registerService,
+            ITemplateMethodParamService<ServiceResponse<GetUserResDTO>, GetUserReqDTO> templateLoggingService)
+        {
+            _getUserService = registerService;
+            _templateLoggingService = templateLoggingService;
+        }
+        // GET: api/<GetUserController>
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<GetUserResDTO>>> Get(GetUserReqDTO reqDTO)
+        {
+            var response = await _templateLoggingService.TemplateMethod(
+                reqDTO,
+                LogsUtils.GetCurrentAsyncMethodName(),
+                _getUserService.GetUser);
+
+            return Ok(response);
+        }
+    }
+}
--- IMS.EF.NET3.1/IMS_Services_GetUser/Startup.cs
+++ IMS.EF.NET3.1/IMS_Services_GetUser/Startup.cs
@@ -1,7 +1,10 @@
+using IMS_DAL_EF_GetUserService;
+using IMS_DAL_GstUserContracts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +13,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TemplateMethodContracts;
+using TemplateMethodService;
 
 namespace IMS_Services_GetUser
 {
@@ -25,11 +30,21 @@ namespace IMS_Services_GetUser
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<GetUserContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped(typeof(IDalGetUserService), typeof(DalEFGetUserService));
+            services.AddScoped(typeof(ITemplateMethodParamService<,>), typeof(TemplateMethodParamServiceImpl<,>));
+
+            services.AddSingleton<ILogger>(provider => provider.GetRequiredService<ILogger<object>>());
+            services.AddSingleton<IConfiguration>(provider => Configuration);
+            services.AddSingleton(MappingConfig.RegisterMaps().CreateMapper());
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
@@ -46,6 +61,9 @@ namespace IMS_Services_GetUser
             {
                 endpoints.MapControllers();
             });
+
+            loggerFactory.AddLog4Net();
+
         }
     }
 }
